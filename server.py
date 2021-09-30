dc1b47e239bcb64cdc68ba12cdb492df1fcff67b
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2021-09-30 21:25:50 +0200

    Pool connections

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import pymodbus.datastore
 import pymodbus.server.sync
 import pymodbus.transaction
 import requests
+import requests.exceptions
 
 FORMAT = (
     "%(asctime)-15s %(threadName)-15s"
@@ -14,6 +15,16 @@ logging.basicConfig(format=FORMAT)
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+_SESSION = None
+
+
+def session() -> requests.Session:
+    """Global session singleton, to pool connections"""
+    global _SESSION
+    if _SESSION is None:
+        _SESSION = requests.Session()
+    return _SESSION
+
 
 def _trigger(address: int, value: int, host="http://localhost") -> None:
     """Process incoming event"""
@@ -21,9 +32,10 @@ def _trigger(address: int, value: int, host="http://localhost") -> None:
     if value == 0:
         return
 
-    # TODO: proper translation
+    # TODO: proper address translation
+    relay = "2_16"
     try:
-        response = requests.get(f"{host}/json/relay/2_16")
+        response = session().get(f"{host}/json/relay/{relay}")
         response.raise_for_status()
     except requests.exceptions.HTTPError as error:
         logger.debug(f"Issue with API call: {error}")
@@ -37,7 +49,7 @@ def _trigger(address: int, value: int, host="http://localhost") -> None:
     # Flip the bit from current by XOR
     toggled = current ^ 0x1
     try:
-        requests.post(f"{host}/json/relay/2_16", json={"value": str(toggled)})
+        session().post(f"{host}/json/relay/{relay}", json={"value": str(toggled)})
         response.raise_for_status()
     except requests.exceptions.HTTPError as error:
         logger.debug(f"Issue with API call: {error}")
